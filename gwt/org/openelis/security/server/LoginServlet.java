bf0e2826197295c542f483f3cbee2a928cc11c3e
dshirazi <dari-shirazi@users.noreply.github.com>
2015-05-11 15:17:28 +0000

    Modified login to check locale before setting it in session.

--- gwt/org/openelis/security/server/LoginServlet.java
+++ gwt/org/openelis/security/server/LoginServlet.java
@@ -60,7 +60,7 @@ public class LoginServlet extends HttpServlet {
     
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException,IOException {
-        String error = null,locale = "en";
+        String locale = "en";
         HttpServletRequest hreq = (HttpServletRequest)req;
 
         //
@@ -68,7 +68,14 @@ public class LoginServlet extends HttpServlet {
         //
         if (hreq.getParameter("locale") != null) {
             locale = req.getParameter("locale");
-            hreq.getSession().setAttribute("locale", locale);
+            switch (locale) {
+                case "fr":
+                    hreq.getSession().setAttribute("locale", "fr");
+                    break;
+                case "en":
+                default:
+                    hreq.getSession().setAttribute("locale", "en");
+            }
         }
 
         //
