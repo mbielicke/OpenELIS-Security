9c610c55a6c316cfea74dd7bd4d6281fee3022ee
akampoow <akampoow@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2019-07-29 20:35:07 +0000

    Fixed the bug where going to the next page on System User screen in Security was locking up the screen. See ticket #1215.

--- OpenELIS/version2/SecurityUI/trunk/gwt/org/openelis/security/modules/application/client/ApplicationScreen.java
+++ OpenELIS/version2/SecurityUI/trunk/gwt/org/openelis/security/modules/application/client/ApplicationScreen.java
@@ -566,6 +566,7 @@ public class ApplicationScreen extends Screen {
                 ApplicationService.get().query(query, new AsyncCallback<ArrayList<IdNameVO>>() {
                     public void onSuccess(ArrayList<IdNameVO> result) {
                         setQueryResult(result);
+                        select(0);
                     }
 
                     public void onFailure(Throwable error) {
