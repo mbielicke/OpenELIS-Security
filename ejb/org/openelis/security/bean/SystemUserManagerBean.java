f6000fdb3b8fe04eee71a7ade9fa7f6623fbd336
mbielick <21997123+mbielicke@users.noreply.github.com>
2020-01-06 17:31:15 +0000

    Cleaned/Updated ivy.xml to more efficiently included needed jars and add support for Wildfly 14.

--- ejb/org/openelis/security/bean/SystemUserManagerBean.java
+++ ejb/org/openelis/security/bean/SystemUserManagerBean.java
@@ -35,7 +35,7 @@ import javax.annotation.security.RolesAllowed;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
-import org.jboss.security.annotation.SecurityDomain;
+import org.jboss.ejb3.annotation.SecurityDomain;
 import org.openelis.security.domain.DataObject;
 import org.openelis.security.domain.ReferenceTable;
 import org.openelis.security.domain.SystemUserModuleViewDO;
